bb87173801697c227f46cf0c1ed65482a270b4bc
LopesA95 <andersonlopes017.al@gmail.com>
2024-05-20 11:38:37 -0400

    ✨ feat: Add Sonner package for toast notifications
    The code changes include adding the Sonner package for toast notifications in the `App.tsx` file. This package will be used to display success and error messages when signing in.

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,15 @@ import { Helmet, HelmetProvider } from 'react-helmet-async'
 import { RouterProvider } from 'react-router-dom'
 import { router } from './routes'
 
+import { Toaster } from 'sonner'
 import './global.css'
 
 export function App() {
 
   return (
     <HelmetProvider>
-      <Helmet titleTemplate='%s | pizza.shop'/>
+      <Helmet titleTemplate='%s | pizza.shop' />
+      <Toaster richColors/>
       <RouterProvider router={router} />
     </HelmetProvider>
   )
--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -5,6 +5,8 @@ import { z } from "zod"
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import { Label } from "@radix-ui/react-label"
+import { Link } from "react-router-dom"
+import { toast } from "sonner"
 
 
 const sininForm = z.object({
@@ -13,19 +15,33 @@ const sininForm = z.object({
 
 type SigninForm = z.infer<typeof sininForm>
 
-export function Signin() {
+export function SignIn() {
     const { register, handleSubmit, formState: { isSubmitting } } = useForm<SigninForm>()
 
     async function handleSignin(data: SigninForm) {
-        console.log(data)
+        try {
 
-        await new Promise(resolve => setTimeout(resolve, 2000))
+            await new Promise(resolve => setTimeout(resolve, 2000))
+
+            toast.success('Enviamos um link de autenticação para o seu e-mail.', {
+                action: {
+                    label: 'Reenviar',
+                    onClick: () =>
+                        handleSignin(data),
+                }
+            })
+        } catch {
+            toast.error('Ocorreu um erro ao enviar o e-mail de autenticação. Tente novamente mais tarde.')
+        }
     }
 
     return (
         <>
             <Helmet title='Login' />
             <div className='p-8'>
+                <Button variant='ghost' asChild className="absolute right-8 top-8">
+                    <Link to="/sign-up">Novo estabelecimento</Link>
+                </Button>
                 <div className="w-[350px] flex flex-col justify-center gap-6">
                     <div className="flex flex-col gap-2 text-center">
                         <h1 className="text-2xl font-semibold tracking-tight">Acessar painel</h1>
