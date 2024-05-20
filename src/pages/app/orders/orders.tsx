e6c07e37a36b52e21fd58ea82f177423e3bb3150
Anderson Lopes <andersonlopes017.al@gmail.com>
2024-05-20 15:12:23 -0400

    ✨ chore: Add @radix-ui/react-select package and update orders page

--- src/pages/app/orders/orders.tsx
+++ src/pages/app/orders/orders.tsx
@@ -1,8 +1,7 @@
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
-import { ArrowRight, Search, X } from "lucide-react";
+import { Table, TableBody, TableHead, TableHeader, TableRow } from "@/components/ui/table";
 import { Helmet } from "react-helmet-async";
+import { OrderTableFiltes } from "./order-table-filters";
+import { OrdersTableRow } from "./orders-table-row";
 
 export function Orders() {
     return (
@@ -12,10 +11,7 @@ export function Orders() {
                 <h1 className="text-3xl font-bold tracking-tight">Pedidos</h1>
             </div>
             <div className="space-y-2.5">
-                <form className="flex items-center gap-2">
-                    <span className="text-sm font-semibold">Filtros:</span>
-                    <Input placeholder="Nome do cliente" className="h-8 w-[320px]" />
-                </form>
+                <OrderTableFiltes />
 
                 <div className="border rounded-md">
                     <Table>
@@ -35,39 +31,7 @@ export function Orders() {
                         <TableBody>
                             {Array.from({ length: 10 }).map((_, i) => {
                                 return (
-                                    <TableRow key={i}>
-                                        <TableCell>
-                                            <Button variant="outline" size="xs">
-                                                <Search className="h-3 w-3" />
-                                                <span className="sr-only">Detalhes do pedido
-                                                </span>
-                                            </Button>
-                                        </TableCell>
-                                        <TableCell className="font-mono text-xs font-medium">313121ni1h32i1nk3ln</TableCell>
-                                        <TableCell className="text-muted-foreground"> há 15 minutos
-                                        </TableCell>
-                                        <TableCell className="flex items-center gap-2">
-                                            <span className="h-2 w-2 rounded-full bg-slate-400" />
-                                            <span className="font-medium text-muted-foreground p-1">
-                                                Pendente
-                                            </span>
-                                        </TableCell>
-                                        <TableCell className="font-medium">Anderson da Silva Lopes
-                                        </TableCell>
-                                        <TableCell className="font-medium">
-                                            R$ 149,90
-                                        </TableCell>
-                                        <TableCell><Button variant='outline' size='xs'>
-                                            <ArrowRight className="h-3 w-3 mr-2" />
-                                            Aprovar
-                                        </Button>
-                                        </TableCell>
-                                        <TableCell><Button variant='ghost' size='xs'>
-                                            <X className="h-3 w-3 mr-2" />
-                                            Cancelar
-                                        </Button>
-                                        </TableCell>
-                                    </TableRow>
+                                    <OrdersTableRow key={i} />
                                 )
                             })}
                         </TableBody>
