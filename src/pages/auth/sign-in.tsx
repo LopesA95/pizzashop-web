be375d3c984a7bc8908bb96467c6e823735bfbc3
LopesA95 <andersonlopes017.al@gmail.com>
2024-05-20 10:49:49 -0400

    ✨ chore: Update dependencies and configuration files and config react hook form with zod

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -1,10 +1,49 @@
 import { Helmet } from "react-helmet-async"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
+
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import { Label } from "@radix-ui/react-label"
+
+
+const sininForm = z.object({
+    email: z.string().email()
+})
+
+type SigninForm = z.infer<typeof sininForm>
 
 export function Signin() {
+    const { register, handleSubmit, formState: { isSubmitting } } = useForm<SigninForm>()
+
+    async function handleSignin(data: SigninForm) {
+        console.log(data)
+
+        await new Promise(resolve => setTimeout(resolve, 2000))
+    }
+
     return (
         <>
             <Helmet title='Login' />
-            <h1>Signin</h1>
+            <div className='p-8'>
+                <div className="w-[350px] flex flex-col justify-center gap-6">
+                    <div className="flex flex-col gap-2 text-center">
+                        <h1 className="text-2xl font-semibold tracking-tight">Acessar painel</h1>
+                        <p className="text-sm text-muted-foreground">Acompanhe suas vendas pelo painel do parceiro
+                        </p>
+                    </div>
+
+                    <form onSubmit={handleSubmit(handleSignin)} className="space-y-4">
+                        <div className="space-y-2">
+                            <Label htmlFor="email">Seu e-mail</Label>
+                            <Input type="email" id="email" placeholder="Seu e-mail" {...register('email')} />
+
+                        </div>
+
+                        <Button disabled={isSubmitting} className="w-full" type="submit">Acessar painel</Button>
+                    </form>
+                </div>
+            </div>
         </>
     )
 }
