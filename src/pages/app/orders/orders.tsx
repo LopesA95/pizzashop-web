c586202fc669637d2a56a424bf1f05ac25604d5e
Anderson Lopes <andersonlopes017.al@gmail.com>
2024-05-22 14:09:52 -0400

    ✨ feat: Add lazy loading for images in Pagination component

--- src/pages/app/orders/orders.tsx
+++ src/pages/app/orders/orders.tsx
@@ -1,18 +1,36 @@
 import { getOrders } from '@/api/get-orders'
 import { Pagination } from "@/components/pagination"
 import { useQuery } from '@tanstack/react-query'
+import { z } from 'zod'
 
 import { Table, TableBody, TableHead, TableHeader, TableRow } from "@/components/ui/table"
 import { Helmet } from "react-helmet-async"
+import { useSearchParams } from 'react-router-dom'
 import { OrderTableFiltes } from "./order-table-filters"
 import { OrdersTableRow } from "./orders-table-row"
 
 
 export function Orders() {
+  const [searchParams, setSearchParams] = useSearchParams()
+
+  const pageIndex = z.coerce
+    .number()
+    .transform((page) => page - 1)
+    .parse(searchParams.get('page') ?? '1')
+
+
   const { data: result } = useQuery({
-    queryKey: ['orders'],
-    queryFn: getOrders,
+    queryKey: ['orders', pageIndex],
+    queryFn: () => getOrders({ pageIndex }),
   })
+
+  function handlepaginate(pageIndex: number) {
+    setSearchParams(state => {
+      state.set('page', (pageIndex + 1).toString())
+
+      return state
+    })
+  }
 
   return (
     <>
@@ -40,12 +58,15 @@ export function Orders() {
               </TableHeader>
               <TableBody>
                 {result && result.orders.map(order => {
-                  return <OrdersTableRow key={order.orderId} order={order}/>
+                  return <OrdersTableRow key={order.orderId} order={order} />
                 })}
               </TableBody>
             </Table>
           </div>
-          <Pagination pageIndex={0} totalCount={105} perpage={10} />
+          {result &&
+            <Pagination
+              onPageChange={handlepaginate}
+              pageIndex={result.meta.pageIndex} totalCount={result.meta.totalCount} perpage={result.meta.perPage} />}
         </div>
       </div>
     </>
