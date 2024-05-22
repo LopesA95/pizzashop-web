b4dd9d9a9fc1aa8f2a99a5d9b825b53444b81b99
Anderson Lopes <andersonlopes017.al@gmail.com>
2024-05-21 22:09:35 -0400

    ✨ feat: Add lazy loading for images in AccountMenu component
    This commit adds lazy loading functionality for images in the AccountMenu component. It imports the Skeleton component from the ui/skeleton module and uses it to display a loading skeleton while the profile and managed restaurant data is being fetched. This change improves the user experience by reducing the initial load time of the AccountMenu component.

--- src/components/account-menu.tsx
+++ src/components/account-menu.tsx
@@ -4,15 +4,16 @@ import { useQuery } from '@tanstack/react-query'
 import { Building, ChevronDown, LogOut } from "lucide-react"
 import { Button } from "./ui/button"
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from "./ui/dropdown-menu"
+import { Skeleton } from './ui/skeleton'
 
 export function AccountMenu() {
 
-  const { data: profile } = useQuery({
+  const { data: profile, isLoading: isLoadingProfile } = useQuery({
     queryKey: ['profile'],
     queryFn: getProfile,
   })
 
-  const { data: managedRestaurant } = useQuery({
+  const { data: managedRestaurant, isLoading: isLoadingManagerRestaurant } = useQuery({
     queryKey: ['managed-restaurant'],
     queryFn: getManagedRestaurant,
   })
@@ -21,14 +22,24 @@ export function AccountMenu() {
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
         <Button variant="outline" className="flex items-center gap-2 select-none">
-          {managedRestaurant?.name || 'Meu perfil'}
+          {isLoadingManagerRestaurant ?
+            <Skeleton className='h-4 w-40' /> : managedRestaurant?.name}
           <ChevronDown className="h-4 w-4" />
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent align="end" className="w-56">
         <DropdownMenuLabel className="flex flex-col">
-          <span>{profile?.name}</span>
-          <span className="text-xs font-normal text-muted-foreground">{profile?.email}</span>
+          {isLoadingProfile ? (
+            <div className='space-y-1.5 '>
+              <Skeleton className='h-4 w-32' />
+              <Skeleton className='h-3 w-24' />
+            </div>
+          ) : (
+            <>
+              <span>{profile?.name}</span>
+              <span className="text-xs font-normal text-muted-foreground">{profile?.email}</span>
+            </>
+          )}
         </DropdownMenuLabel>
         <DropdownMenuSeparator />
         <DropdownMenuItem>
